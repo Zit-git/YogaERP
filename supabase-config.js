4e57d04602ac0d409d52030c6fc97f253414dbf9
Zit-git <59389785+Zit-git@users.noreply.github.com>
2026-06-17 23:16:36 +0530

    Point app to active Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 globalThis.ALIYAR_SUPABASE = {
-  url: "https://eghwgxkcpcvxavtumdxa.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnaHdneGtjcGN2eGF2dHVtZHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2OTI1MTQsImV4cCI6MjA5NzI2ODUxNH0.1NZpIUdYDYsX_ZEQ4oaJaj1Ke3Y98TQZVZ9igim8PuY"
+  url: "https://jzwtgazgbcbxnzylaxbm.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6d3RnYXpnYmNieG56eWxheGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NzA5MjMsImV4cCI6MjA5NzI0NjkyM30.gYPBcYBu4eDfApmeNly0_3AjMlrvYpFbQyHeVkGJ1L0"
 };
 window.ALIYAR_SUPABASE = globalThis.ALIYAR_SUPABASE;
